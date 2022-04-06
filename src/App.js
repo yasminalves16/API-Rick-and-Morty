df6dcaf64d223390ecbd42decbb8afc197e6db9e
YasminAlves16 <yasmin.alves16@outlook.com.br>
2022-04-06 03:41:31 -0300

    looking for more characters, created button next and prev

--- src/App.js
+++ src/App.js
@@ -5,24 +5,39 @@ import Chacaters from "./Components/Characters";
 function App() {
   const [charactersList, setCharactersList] = useState([]);
 
-  const [currentPage, setcurrentPage] = useState('https://rickandmortyapi.com/api/character/.')
+  const [currentPage, setcurrentPage] = useState(1);
 
   useEffect(() => {
     const fetchResponse = async () => {
-      const response = await fetch({currentPage})
+      const response = await fetch(
+        `https://rickandmortyapi.com/api/character/?page=${currentPage}`
+      );
 
       const jsonResponse = await response.json();
       setCharactersList(jsonResponse.results);
-    }
+    };
 
     fetchResponse();
-  }, [])
+  }, [currentPage]);
 
-  
+  const previousPage = () => {
+    if (currentPage > 1) {
+      setcurrentPage(currentPage - 1);
+    }
+  };
+
+  const nextPage = () => {
+    if (currentPage <= 42) {
+      setcurrentPage(currentPage + 1);
+    }
+  };
+
+
   return (
     <div>
       <Chacaters charactersList={charactersList} />
-      <button onClick={}></button>
+      <button onClick={previousPage}>Previous</button>
+      <button onClick={nextPage}>Next</button>
     </div>
   );
 }
